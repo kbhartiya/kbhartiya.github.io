4caeaa2085290e2639de70cb5ab6472bac3f2e08
kbhartiya <kbhartiya83@gmail.com>
2020-04-12 03:00:13 +0530

    Updates

---
+++ precache-manifest.7ffa215cca92c52d1233516eeba26e05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc1140479674f954dd3e6df6bd55c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca04e062c555e30df5a5",
+    "url": "/static/css/main.2676cf61.chunk.css"
+  },
+  {
+    "revision": "0cbd8d2722abaf655976",
+    "url": "/static/js/2.fe92f48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fe92f48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca04e062c555e30df5a5",
+    "url": "/static/js/main.8d2b9e6f.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "0a4eb1aec2f77f051c803c41ac3a7879",
+    "url": "/static/media/dust-and-scratches.0a4eb1ae.jpg"
+  },
+  {
+    "revision": "41dfaa73306492a8d784a8cb0eaed95a",
+    "url": "/static/media/kartik.41dfaa73.jpg"
+  }
+]);
